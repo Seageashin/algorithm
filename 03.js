c1362431f71f0203fa8ad7ee08095ac9cb165970
세계 <nw0417@gmail.com>
2024-05-10 19:07:25 +0900

    배열의평균값

---
+++ 03.js
@@ -0,0 +1,5 @@
+function solution(arr) {
+    return arr.reduce((sum, current) => sum + current) / arr.length;
+}
+
+//   정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
